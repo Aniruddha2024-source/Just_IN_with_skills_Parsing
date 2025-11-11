cead3f09bb748ad6db29ace0783bbb312464b86f
Aniruddha <aniruddhachowdhuru898@gmail.com>
2025-11-12 02:28:44 +0530

    production rady app

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['https://just-in-with-skills-parsing-5.onrender.com/'],
+    origin:['https://just-in-with-skills-parsing-5.onrender.com'],
     credentials:true
 }
 
